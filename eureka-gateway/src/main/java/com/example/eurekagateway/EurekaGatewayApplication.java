9dad01aef25e8098d4364d5a959fe4bb06c3213d
胡宗旭 <946546993@qq.com>
2021-05-19 14:31:38 +0800

    gateway测试

---
+++ eureka-gateway/src/main/java/com/example/eurekagateway/EurekaGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.eurekagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGatewayApplication.class, args);
+    }
+
+}
