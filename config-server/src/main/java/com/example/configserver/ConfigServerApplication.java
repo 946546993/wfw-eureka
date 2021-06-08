4df293243b50164e6bedb0275fc5884d3f5368c4
胡宗旭 <946546993@qq.com>
2021-06-08 16:11:15 +0800

    配置中心服务端

---
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.example.configserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author hzx
+ */
+
+@EnableConfigServer
+@SpringBootApplication
+@Slf4j
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+        log.info("配置中心启动完成");
+    }
+
+}
