e3e631c768e37de9b6917ce1585e4e0aeb2ed495
胡宗旭 <946546993@qq.com>
2021-06-09 15:00:42 +0800

    配置中心客户端 一个client端配置自动更新 完成

--- config-client/src/main/java/com/example/configclient/controller/GitController.java
+++ config-client/src/main/java/com/example/configclient/controller/GitController.java
@@ -3,6 +3,7 @@ package com.example.configclient.controller;
 import com.example.configclient.config.GitAutoRefreshConfig;
 import com.example.configclient.config.GitConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hzx
  */
 @RestController
+@RefreshScope
 public class GitController {
 
 
@@ -19,11 +21,21 @@ public class GitController {
     @Autowired
     private GitAutoRefreshConfig gitAutoRefreshConfig;
 
+    /**
+     *    “@Value”注解不能实现配置刷新功能
+     * @return
+     */
     @GetMapping(value = "show")
     public Object show(){
         return gitConfig;
     }
 
+    /**
+     * 刷新配置  发送post请求到： http://localhost:8788/actuator/refresh
+     * 后续用到 webhook  当有提交的时候，发送一个接口请求，实现单一client端的配置刷新
+     * 多client 端的配置刷新， 结合webhook + MQ   使用消息广播的形式，可实现多端的配置自动更新
+     * @return
+     */
     @GetMapping(value = "autoShow")
     public Object autoShow(){
         return gitAutoRefreshConfig;
